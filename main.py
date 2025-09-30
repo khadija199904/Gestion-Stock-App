7d3a44fc675e5603226610793cbca6beb9d2cf6b
Assma-IBIKAS <Assma-IBIKAS>
2025-09-30 12:42:28 +0100

    commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from stock import ajt_prd, sup_prd, mdf_qnt,aff_stock
+
+
+ajt_prd()
+sup_prd()
+mdf_qnt()
+aff_stock()
---
+++ stock.py
@@ -0,0 +1,36 @@
+stock = []
+
+def ajt_prd():
+    prd = input("Entrer votre produit sous forme (Produit,Quantite,Prix) : ")
+    nom, qnt, prix_unit = prd.split(",")
+    qnt = int(qnt)
+    prix_unit = float(prix_unit)
+    stock.append([nom, qnt, prix_unit])
+    print(f"{nom} ajouté au stock.")
+
+def sup_prd():
+    nom = input("Entrer le nom de produit à supprimer : ")
+    for p in stock:
+        if p[0] == nom:
+            stock.remove(p)
+            print(f"{nom} supprimé.")
+            return  
+    print("Le produit n'existe pas !!!")
+
+def mdf_qnt():
+    nom = input("Nom du Produit à modifier : ")
+    for p in stock:
+        if p[0] == nom:
+            qnt = int(input("Nouvelle Quantité : "))
+            p[1] = qnt
+            print(f"Quantité de {nom} mise à jour.")
+            return  
+    print("Le produit n'existe pas !!!!")
+
+def aff_stock():
+    if not stock:
+        print("Le stock est vide.")
+        return
+    print("Stock actuel :")
+    for p in stock:
+        print(f"- {p[0]} : quantité={p[1]}, prix={p[2]}")
