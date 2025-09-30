e319ed3232f48cd0cac84845d9dc49d604277265
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-10-01 00:46:49 +0100

    Resolved merge conflicts in main.py and stock.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 from stock import ajt_prd, sup_prd, mdf_qnt,aff_stock, stock
 from stats import stats
 
@@ -19,22 +17,3 @@ while True :
 sup_prd()
 mdf_qnt()
 stats(stock)
-=======
-from stock import ajt_prd,aff_stock,mdf_qnt,sup_prd
-
-
-
-while True :
-    choix = input(" Voulez-vous ajouter un produit ? (o/n)")
-    if choix == "o":
-        ajt_prd()
-        aff_stock()
-    elif choix =="n":
-        print("Fin !!!")
-        break
-    else :
-        print("Réponse invalide !!!")
-
-mdf_qnt()
-sup_prd()
->>>>>>> 6884e05e83bd91543a1d0c6915af1f6180a1cb4a
