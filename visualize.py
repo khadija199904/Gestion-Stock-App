a34664ce4cb7e08475db5a3de72adc7caea48cad
Khadija Elabbioui <khadijaelabbioui1999@gmail.com>
2025-10-05 14:49:16 +0100

    Update: improved stock management logic

--- visualize.py
+++ visualize.py
@@ -3,18 +3,18 @@ import numpy as np
 
 
      
-product = np.array([item[0] for item in stock])
-Qnt = np.array([item[1] for item in stock])
-Unit_Price = np.array([item[2] for item in stock])
+
      
-# Valeur par produit
-valeurs = Unit_Price * Qnt
-#Graphique en barres : quantité par produit.
+
 
 def visualize_bar(stock) :
      if not stock:
         print("⚠️ Le stock est vide.")
         return
+     product = np.array([item[0] for item in stock])
+     Qnt = np.array([item[1] for item in stock])
+     Unit_Price = np.array([item[2] for item in stock])
+
      plt.figure(figsize=(10, 6))
      plt.bar(product,Qnt,color='skyblue')
      plt.xlabel('Quantité')
@@ -26,9 +26,12 @@ def visualize_pie(stock) :
      if not stock:
         print("⚠️ Le stock est vide.")
         return
-     # Tracé du camembert
+     # Valeur par produit
+     valeurs = Unit_Price * Qnt
+
+     #Graphique en barres : quantité par produit.
      plt.figure(figsize=(6, 6))
-     plt.pie(valeurs, labels=product, autopct="%1.1f%%", startangle=140)
+     plt.pie(valeurs, labels=Product, autopct="%1.1f%%", startangle=140)
      plt.title("Répartition de la valeur totale du stock par produit")
      plt.show()
 
